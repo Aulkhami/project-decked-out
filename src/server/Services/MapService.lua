82fbae139eb1594c6ddd7b540731a36f58f098f8
Rakha030806 <rakha.tblt@gmail.com>
2021-04-17 16:23:40 +0700

    Added MapService
    MapService is a service that manages maps. Self-explanatory ya know.

---
+++ src/server/Services/MapService.lua
@@ -0,0 +1,51 @@
+local Knit = require(game:GetService("ReplicatedStorage").Knit)
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local DungeonReplicated = ReplicatedStorage.Dungeon
+local Dungeon = workspace.Dungeon
+
+local MapService = Knit.CreateService {
+    Name = "MapService";
+    Client = {};
+    LoadedMap = {};
+}
+
+
+function MapService:LoadMap(map)
+    local mapDatabase = {
+        -- Sands of Beginnings
+        ["SoBL01"] = {
+            map = DungeonReplicated.A1SoB.SoBL01;
+            mapDir = Dungeon.A1SoB.SoBL01
+        };
+    }
+
+    -- Getting NewMap from the Map Database and Cloning it into Workspace
+    local newMap = mapDatabase[map].map:Clone()
+    newMap.Parent = mapDatabase[map].mapDir
+    -- Getting the NewMap's ID
+    local mapID
+    while true do
+        mapID = math.random(1000,9999)
+        if not table.find(self.LoadedMap,mapID) then
+            break
+        end
+    end
+    newMap.Name = mapID
+    
+    -- Adding newMap to LoadedMap
+    self.LoadedMap[mapID] = newMap
+    print(self.LoadedMap)
+end
+
+
+function MapService:KnitStart()
+    self:LoadMap("SoBL01")
+end
+
+
+function MapService:KnitInit()
+    
+end
+
+
+return MapService
--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,5 +1,6 @@
 local Knit = require(game:GetService("ReplicatedStorage").Knit)
 
 Knit.AddServices(script.Services)
+print("Adding Services")
 
 Knit:Start():Catch(warn):Await()
